8aa5176ca95cca34c88a42fcad2ec7dc13c88abf
kimminna <kmina121777@naver.com>
2025-09-25 22:32:53 +0900

    ✨ feat: 홈 페이지에 도서 목록 표시 기능 추가

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -4,15 +4,15 @@ import books from '@/mocks/books.json';
 export default function Home() {
   return (
     <div>
-      <section>
-        <h3>지금 추천하는 도서</h3>
+      <section className='mb-5 flex flex-col gap-5'>
+        <h3 className='text-xl font-bold'>지금 추천하는 도서</h3>
         {books.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
 
-      <section>
-        <h3>등록된 모든 도서</h3>
+      <section className='flex flex-col gap-5'>
+        <h3 className='text-xl font-bold'>등록된 모든 도서</h3>
         {books.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
