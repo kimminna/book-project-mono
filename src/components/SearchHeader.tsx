1d87c74cb1158f2611a59701465a6b7f5608e526
kimminna <kmina121777@naver.com>
2025-09-26 23:44:01 +0900

    ✨ feat: 검색 기능 개선

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -1,5 +1,5 @@
 'use client';
-import {useRouter} from 'next/navigation';
+import {useRouter, useSearchParams} from 'next/navigation';
 // 서버 vs 클라이언트 컴포넌트 구분
 // 특정 컴포넌트가 상호작용이 있는지?
 // 입력, 클릭과 같은 상호작용은 클라이언트 컴포넌트로
@@ -13,25 +13,39 @@ import {useRouter} from 'next/navigation';
 // 정적페이지 - 빌드 타임에 미리 생성된 페이지(like SSG)
 // 동적페이지 - 브라우저의 요청을 받을 때마다 생성되는 페이지 (like SSR)
 
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 export const SearchHeader = () => {
   const [search, setSearch] = useState<string>('');
   const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const q = searchParams.get('q');
+
+  useEffect(() => {
+    setSearch(q || '');
+  }, [q]);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
   const handleSubmit = () => {
+    if (!search || q === search) return;
     router.push(`/search?q=${search}`);
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  };
   return (
     <div className='mt-4 flex h-12 gap-2'>
       <input
         value={search}
         onChange={handleSearch}
+        onKeyDown={handleKeyDown}
         className='flex-1 rounded-[5px] border-[1px] border-gray-300 p-[15px] outline-none'
       />
       <button
