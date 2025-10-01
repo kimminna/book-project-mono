a0d4123589891460a8b74def8b46393fa619a1af
kimminna <kmina121777@naver.com>
2025-10-01 13:54:57 +0900

    ✨ feat: 개선된 API 호출 및 검색 파라미터 처리

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -3,7 +3,8 @@ import {BookData} from '@/types/book';
 
 async function AllBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    {cache: 'force-cache'}
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다.</div>;
@@ -20,7 +21,8 @@ async function AllBooks() {
 }
 async function RecommendedBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    {next: {revalidate: 3}}
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다.</div>;
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -4,18 +4,23 @@ import {BookData} from '@/types/book';
 export default async function Page({
   searchParams,
 }: {
-  searchParams: {q?: string};
+  searchParams?: {[key: string]: string | string[] | undefined};
 }) {
+  const query = searchParams?.query ?? '';
+
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${query}`
   );
+
   if (!response.ok) {
     return <div>오류가 발생했습니다.</div>;
   }
-  const books = await response.json();
+
+  const books: BookData[] = await response.json();
+
   return (
     <div>
-      {books.map((book: BookData) => (
+      {books.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
     </div>
