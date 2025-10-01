2f80c66caf6f900ae2fc9df9dee0e825dbc3ebd6
kimminna <kmina121777@naver.com>
2025-10-01 13:55:30 +0900

    ✨ feat: add logging configuration for fetches in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
-import type { NextConfig } from "next";
-import path from "path";
+import type {NextConfig} from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias ?? {}),
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     };
     return config;
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
   },
 };
 
